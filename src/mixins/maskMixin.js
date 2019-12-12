90b4387c3d9ae08629616d7713b8018c4a9f10a2
Unknown <ludovic.cottret@toulouse.inra.fr>
2019-12-12 14:23:22 +0100

    Mask Mixin

---
+++ src/mixins/maskMixin.js
@@ -0,0 +1,38 @@
+import * as d3 from "d3";
+
+export const maskMixin = {
+  props: {
+    seqs: {
+      type: Array,
+      default: () => {
+        return null;
+      }
+    },
+    width: {
+      type: Number,
+      default: 600
+    },
+    height: {
+      type: Number,
+      default: 300
+    }
+  },
+  computed: {
+    xScale() {
+      return d3
+        .scaleLinear()
+        .range([0, this.width])
+        .domain([0, this.maxLength]);
+    },
+    yScale() {
+      return d3
+        .scaleLinear()
+        .range([0, this.height])
+        .domain([0, this.seqs.length]);
+    },
+    maxLength() {
+      let a_lengths = this.seqs.map(seq => seq.seq.length);
+      return d3.max(a_lengths);
+    }
+  }
+};
